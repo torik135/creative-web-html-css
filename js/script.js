66242ff7d80d67e80c332f468bc7c1102ba3d1ad
torik <riqibrekele@gmail.com>
2021-12-08 23:14:28 +0700

    add services and home styling

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
+// show nav
 const toggle = document.querySelector('.toggle')
 const nav = document.querySelector('.navigation')
-
 
 const toggleEvent = () => {
 	toggle.classList.toggle('active')
 	nav.classList.toggle('active')
 }
 
-
 toggle.addEventListener('click', toggleEvent)
+
+
+// blur fx
+const homeSec = document.querySelector('.home-section')
+
+
+var load = 0
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return (
+		(num - in_min) * (out_max - in_max) / (in_max - in_min) + out_min
+		)
+}
+
+
+const blurring = () => {
+	load++
+
+	if (load > 99) {
+		clearInterval(int)
+	}
+	homeSec.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`
+
+}
+
+var int = setInterval(blurring, 30)
